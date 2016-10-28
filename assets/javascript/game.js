ba187c581e9576a6b0099181d9b54f9dc7fdfdcc
Christine Sizemore <christine.e.sizemore@gmail.com>
2016-10-27 20:25:26 -0500

    game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,17 @@
 $(function(){
 
+	/* Instantiate global vars */
+	var player, enemy;
+	var fadeTime = 800;
+
 	/* Initiate HP bars */
 	$('.hp-bar').each(function(){
 		var hp = $(this).parent().data('hp');
 		$(this).find('span').html(hp + ' HP');
 	});
 
-	/* Instantiate global vars */
-	var player;
-	var enemy;
-	var fadeTime = 800;
-
 	/* Create character class */
-	var Character = function(id, hp, atk, def){
+	var Character = function(id, hp, atk, def, win, lose){
 		this.id = id;
 		this.currentHp = hp;
 		this.totalHp = hp;
@@ -21,26 +20,109 @@ $(function(){
 		this.counterAttack = def;
 	}
 	/* Character takes a hit */
-	Character.prototype.fight = function(cpu) {
-		this.currentHp -= cpu.counterAttack;
-		cpu.currentHp -= this.currentAttack;
+	Character.prototype.fight = function() {
+		// Temporarily disable button
+		$('#btn-fight').prop('disabled', true).addClass('disabled');
+
+		this.currentHp -= enemy.counterAttack;
+		enemy.currentHp -= this.currentAttack;
 		this.currentAttack += this.baseAttack;
-		console.log(this);
+
+		// Update HP display
 		this.updateHp();
-		cpu.updateHp();
+		enemy.updateHp();
+
+		// End fight if either HP <= 0
+		if(this.currentHp <= 0){
+			// Game over!
+			showLose();
+		}else if(enemy.currentHp <= 0){
+			// Check for other enemies
+			hasEnemy = $('#select-screen .char-box').length;
+
+			if(hasEnemy){ // There are more enemies
+				// Empty enemy variable
+				enemy = null;
+				// Select another enemy
+				showEnemies();
+			}else{ // You win!
+				showWin();
+			}
+		}else{
+			// Still fighting. Re-enable the fight button.
+			$('#btn-fight').prop('disabled', false).removeClass('disabled'); 
+		}
 	}
+
 	/* Update the HP bar */
 	Character.prototype.updateHp = function(){
-		$('#'+this.id).find('.hp-num').html(this.currentHp + ' / ' + this.totalHp + ' HP');
-		$('#'+this.id).find('.hp-fill').width(this.currentHp / this.totalHp * 100 + '%');
+		var hpText = this.currentHp + ' / ' + this.totalHp + ' HP';
+		var hpFill = this.currentHp / this.totalHp * 100 + '%';
+
+		$('#'+this.id).find('.hp-text').html(hpText);
+		$('#'+this.id).find('.hp-fill').width(hpFill);
+		
+		// Die if HP <= 0
+		if(this.currentHp <= 0){
+			this.lose();
+		}
 	}
+
+	/* Character die */
+	Character.prototype.lose = function(){
+		$('#'+this.id).addClass('dead');
+	}
+
 
 	// The function to assign data to a new Character
 	function selectChar(id){
 		char = $('#'+id);
-		charObj = new Character(id, char.data('hp'), char.data('atk'), char.data('def'));
+		charObj = new Character(
+			id,
+			char.data('hp'),
+			char.data('atk'),
+			char.data('def'),
+			char.find('.win-text'),
+			char.find('.lose')
+		);
 		charObj.html = char;
 		return charObj;
+	}
+	function showEnemies(){
+		$('#fight-screen').fadeOut(fadeTime,function(){
+			// Ensure the fight button is enabled
+			$('#btn-fight').prop('disabled', false).removeClass('disabled'); 
+			// Show the fight screen
+			$('#select-screen').fadeIn();
+		});
+	}
+	function showFight(){
+		$('#select-screen').fadeOut(fadeTime,function(){
+			// Add enemy to fighter box
+			$('#fight-screen #div-enemy').html(enemy.html);
+			// Make sure player & enemy are unfaded
+			$('#fight-screen').find('.char-box').show();
+			// Show the fight screen
+			$('#fight-screen').fadeIn();
+		});
+	}
+	function showWin(){
+		$('#fight-screen').fadeOut(fadeTime,function(){
+			$('#player-win').html(player.html);
+			$('.hp-bar').hide();
+			$('.char-win').removeClass('noshow');
+			// Show the win screen
+			$('#win-screen').fadeIn();
+		});
+	}
+	function showLose(){
+		$('#fight-screen').fadeOut(fadeTime,function(){
+			$('#player-lose').html(player.html);
+			$('.hp-bar').hide();
+			$('.char-lose').removeClass('noshow');
+			// Show the lose screen
+			$('#lose-screen').fadeIn();
+		});
 	}
 
 	// Select characters as player and defender
@@ -58,30 +140,27 @@ $(function(){
 			// Hide player from selection
 			$(this).fadeOut(fadeTime,function(){
 				// Add player to player box
-				$('#fight #div-player').html(player.html);
+				$('#fight-screen #div-player').html(player.html);
 			});
-		}else if(!enemy){ // Defender not selected yet
+		}else if(!enemy && this.id != player.id){ // Prevent double click
+			// Defender not selected yet
 			enemy = selectChar(this.id);
 
 			// Fade correct title
 			$('#title-player').fadeOut(fadeTime,function(){
 				$('#title-enemy').fadeIn();
 			});
-			// Hide enemy select screen
-			$(this).parents('#select-screen').fadeOut(fadeTime,function(){
-				// Add enemy to fighter box
-				$('#fight #div-enemy').html(enemy.html);
-				// Make sure player & enemy are unfaded
-				$('#fight').find('.char-box').show();
-				// Show the fight screen
-				$('#fight').fadeIn();
-			});
+			// Hide enemy select screen & show fight
+			showFight();
 		}
 
 	});
 
 	$('#btn-fight').on('click', function(){
-		player.fight(enemy);
+		player.fight();
+	});
+	$('.btn-refresh').on('click', function(){
+		location.reload();
 	});
 
 });
--- index.php
+++ index.php
@@ -12,46 +12,86 @@
 </head>
 <body>
 	<header class="site-header">
-		<h1 class="page-title"><div id="logo"><img src="assets/images/star-trek-tng-logo.png" alt="Star Trek TNG" /></div></h1>
+		<div class="container">
+			<h1 class="page-title"><div id="logo"><img src="assets/images/star-trek-tng-logo.png" alt="Star Trek TNG" /></div></h1>
+		</div>
 	</header>
 	<section id="select-screen" class="section container">
 		<h2 class="section-title" id="title-player">Choose your player</h2>
 		<h2 class="section-title noshow" id="title-enemy">Choose your adversary</h2>
-		<div id="div-chars" class="div-chars clearfix">
+		<div id="div-chars" class="div-chars row">
 			<div class="char-box" id="human" data-hp="150" data-atk="8" data-def="10">
+				<div class="char-win noshow">
+					<h3 class="quote"><q>The line must be drawn here! This far, no farther!</q></h3>
+				</div>
+				<div class="char-lose noshow">
+					<h3 class="quote"><q>It is possible to commit no mistakes and still lose. That is not a weakness; that is life.</q></h3>
+				</div>
 				<div class="char-image"><img src="assets/images/human.png" alt="USS Enterprise and Capt. Picard" /></div>
 				<h3 class="char-name">Human</h3>
-				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-num"></span></div>
+				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-text"></span></div>
 			</div><!--
 
 			--><div class="char-box" id="klingon" data-hp="180" data-atk="10" data-def="5">
+				<div class="char-win noshow">
+					<h3 class="quote"><q>potlhbe’chugh yay qatlh pe’’eghlu’.</q></h3>
+					<h4 class="translate">If winning is not important, why keep score?</h4>
+				</div>
+				<div class="char-lose noshow">
+					<h3 class="quote"><q>Heghlu'meH QaQ jajvam</q></h3>
+					<h4 class="translate">Perhaps it is a good day to die.</h4>
+				</div>
 				<div class="char-image"><img src="assets/images/klingon.png" alt="Bird of Prey and Klingon" /></div>
 				<h3 class="char-name">Klingon</h3>
-				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-num"></span></div>
+				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-text"></span></div>
 			</div><!--
 
 			--><div class="char-box" id="cardassian" data-hp="120" data-atk="10" data-def="12">
+				<div class="char-win noshow">
+					<h3 class="quote"><q>I hope you realize the consequences of Federation officers firing on Cardassian troops!</q></h3>
+				</div>
+				<div class="char-lose noshow">
+					<h3 class="quote"><q>Wasted energy, captain. You might come to wish you hadn't expended it in such a futile effort.</q></h3>
+				</div>
 				<div class="char-image"><img src="assets/images/cardassian.png" alt="Cardassian starship and Cardassian" /></div>
 				<h3 class="char-name">Cardassian</h3>
-				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-num"></span></div>
+				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-text"></span></div>
 			</div><!--
 
 			--><div class="char-box" id="borg" data-hp="220" data-atk="4" data-def="25">
+				<div class="char-win noshow">
+					<h3 class="quote"><q>We are Borg. Resistance is futile.</q></h3>
+				</div>
+				<div class="char-lose noshow">
+					<h3 class="quote"><q>You can't begin to imagine the life you denied yourself.</q></h3>
+				</div>
 				<div class="char-image"><img src="assets/images/borg.png" alt="Borg cube and Borg drone" /></div>
 				<h3 class="char-name">Borg</h3>
-				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-num"></span></div>
+				<div class="hp-bar"><div class="hp-fill"></div><span class="hp-text"></span></div>
 			</div>
 		</div>
 	</section>
-	<section id="fight" class="section container noshow">
+	<section id="fight-screen" class="section container noshow">
 		<h2 class="section-title" id="title-fight">FIGHT!</h2>
-		<div id="div-player" class="col-sm-5">
+		<div class="row row-sm-eq-height">
+			<div id="div-player" class="col-sm-4 col-md-5">
+			</div>
+			<div class="col-sm-4 col-md-2 vcenter">
+				<button id="btn-fight" class="game-btn">ATTACK</button>
+			</div>
+			<div id="div-enemy" class="col-sm-4 col-md-5">
+			</div>
 		</div>
-		<div class="col-sm-2">
-			<button id="btn-fight">ATTACK</button>
-		</div>
-		<div id="div-enemy" class="col-sm-5">
-		</div>
+	</section>
+	<section id="win-screen" class="section container noshow">
+		<h2 class="section-title" id="title-fight">YOU WIN!</h2>
+		<div id="player-win" class="game-end"></div>
+		<button class="btn-refresh game-btn">Play again?</button>
+	</section>
+	<section id="lose-screen" class="section container noshow">
+		<h2 class="section-title" id="title-fight">YOU LOSE!</h2>
+		<div id="player-lose" class="game-end"></div>
+		<button class="btn-refresh game-btn">Try again?</button>
 	</section>
 
 	<!-- Call jQuery and game JS -->
